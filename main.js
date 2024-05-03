871022ce8d859e774dcd807a23682e83e45fb7cb
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2024-05-03 13:12:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+// number
+// string
+// boolean
+
+// {
+//     let a = 'hello okten';
+//     var b = 'hello world';
+// }
+
+// console.log(b);
+// console.log(a);
+
+// const PI = 3.14;
+// console.log(PI); 
+
+let a = 'hello okten';
+a = '5';
+console.log(typeof a, a);
+
+let b = 100500; 
+console.log(typeof b, b);
+
+let c = true;
+console.log(c);
