be79d9a07dc5235aac051b85ee964bf88d7c53c5
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2024-05-13 20:33:09 +0300

    module9.js

--- module8.js
+++ module8.js
@@ -193,3 +193,109 @@ const sortByDescendingPrice = (cars) =>
   [...cars].sort((a, b) => b.price - a.price);
 
 console.table(sortByDescendingPrice(cars));
+
+//* Example 9 - Метод sort
+// Нехай функція sortByModel повертає новий масив автомобілів відсортований за назвою моделі в алфавітному та зворотному алфавітному порядку, в залежності від значення параметра order.
+
+const sortByModel = (cars, order) =>
+  [...cars].sort((a, b) => {
+    switch (order) {
+      case "asc":
+        return a.model.localeCompare(b.model);
+      case "desc":
+        return b.model.localeCompare(a.model);
+    }
+  });
+
+console.table(sortByModel(cars, "asc"));
+console.table(sortByModel(cars, "desc"));
+
+//* Example 10 - Метод reduce
+// Нехай функція getTotalAmount повертає загальну кількість автомобілів (значення властивостей amount).
+
+// const getTotalAmount = (cars) =>
+//   cars.reduce((acc, { amount }) => acc + amount, 0);
+
+// console.log(getTotalAmount(cars));
+
+/*
+1. (acc = 0, { amount = 14 }) => acc + amount | 0 + 14 = 14
+2. (acc = 14, { amount = 2 }) => acc + amount | 14 + 2 = 16
+3. (acc = 16, { amount = 8 }) => acc + amount | 16 + 8 = 24
+*/
+
+/*
+2. (acc = obj, { amount = 2 }) => acc + amount | obj + 2
+3. (acc = obj2, { amount =82 }) => acc + amount | obj2 + 8 = obj28
+*/
+
+//* Example 11 - Ланцюжки методів
+// Нехай функція getAvailableCarNames повертає масив моделей автомобілів, але тільки тих, які зараз на розпродажі.
+
+const getModelsOnSale = (cars) =>
+  cars.filter(({ onSale }) => onSale).map(({ model }) => model);
+
+console.table(getModelsOnSale(cars));
+
+//* Example 12 - Ланцюжки методів
+// Нехай функція getSortedCarsOnSale повертає масив автомобілів на розпродажі (Властивість onSale), відсортованих за зростанням ціни.
+
+const getSortedCarsOnSale = (cars) => cars.filter(({ onSale }) => onSale).sort((a, b) => a.price - b.price);
+
+console.table(getSortedCarsOnSale(cars));
+
+//! ===============
+
+/*
+[]
+.filter()
+.map()
+.sort()
+.find()
+*/ 
+
+// const numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5];
+// const uniqueNumbers = numbers.filter(
+//   (number, index, numbers) => numbers.indexOf(number) === index
+// );
+// console.log(uniqueNumbers);
+
+// console.log(new Set(numbers));
+
+/*
+1. numbers.filter(
+  (number = 1, index = 0, numbers) => numbers.indexOf(number = 1)  === index | 0 === 0 -> true
+);
+2. numbers.filter(
+  (number = 1, index = 1, numbers) => numbers.indexOf(number = 1)  === index | 0 === 1 -> false
+);
+3. numbers.filter(
+  (number = 2, index = 2, numbers) => numbers.indexOf(number = 2)  === index | 2 === 2 -> true
+);
+4. numbers.filter(
+  (number = 2, index = 3, numbers) => numbers.indexOf(number = 2)  === index | 2 === 3 -> false
+
+  [1, 2]
+);
+
+*/
+
+//! ===============
+
+// const subjects = ["Math", "PE", "Chemistry", "Physics"];
+
+// const index = subjects.findIndex((subject) => subject === "Physics");
+// console.log(index);
+
+// const arr = [3, 4, 2, 1, 35, 3, 2, 1, 9, 3, 2];
+
+// const i = arr.findIndex((el) => el > 40);
+
+// console.log(i);
+// console.log(arr[i]);
+
+//! ===============
+
+// (function (msg) {
+//   console.log(msg);
+// })("hello world!");
